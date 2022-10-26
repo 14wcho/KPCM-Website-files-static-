c1d5da43e024662430484abca777d6e9953c2442
14wcho <91303036+14wcho@users.noreply.github.com>
2022-10-25 20:41:20 -0500

    Add files via upload

---
+++ Serve.js
@@ -0,0 +1,13 @@
+const Serve = () => {
+    return myElement;
+  };
+  
+const myElement = (
+    <div>
+    <h1 style={{textIndent: "120px"}}>섬기는 이들</h1>
+    </div>
+);
+
+
+
+export default Serve;
